173acb9fa2e9d12548e3d58152060da1beb1ad55
a.vandam <agustin.vandam@gmail.com>
2022-03-21 15:34:37 -0300

    Create palindrome evaluation function

---
+++ palindrome_evaluation.py
@@ -0,0 +1,24 @@
+from typing import Dict
+
+
+def TestPalindromeFunc():
+    TestDict = {"blablabla": False, "menem":True, "example":False }
+    
+    for word, expected in TestDict.items():
+        result = isPalindrome(word)
+        if result != expected:
+            print("function failed: word: {0}, expected: {1}, received: {2}".format(word, result, expected) )
+        elif result == expected:
+            print("fucntion worked ok for word {0}".format(word))
+    return 
+
+def isPalindrome(word):
+    wordLength = len(word)
+    lastCharPosition = wordLength -1
+    for i in range(0, lastCharPosition//2):
+        if word[i] != word[lastCharPosition-i]:
+            return False
+    return True
+
+TestPalindromeFunc();
+        
